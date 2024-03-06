40129f25a30c0424c591e4abeb4b43ee15a3c2b9
Christofanis Skordas <55315750+skordaschristofanis@users.noreply.github.com>
2024-03-06 17:05:16 -0600

    Switched to static links to fix proxy issues and added a startup script

---
+++ start_voting_app.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# -----------------------------------------------------------------------------
+# Project: SEES-Voting-App
+# File: start_voting_app.sh
+# -----------------------------------------------------------------------------
+# Purpose:
+# This script is used to start the SEES-Voting-App web application.
+#
+# Copyright (C) 2024 GSECARS, The University of Chicago, USA
+# This software is distributed under the terms of the MIT license.
+# -----------------------------------------------------------------------------
+
+source /usr/share/anaconda3/bin/activate
+conda activate votingAppENV
+python /usr/share/sees-voting-app/voting_app.py
